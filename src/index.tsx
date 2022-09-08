b0f98a095db2343f94ef7150654e63a8b81f7a43
Daniil <ralf.danya@mail.ru>
2022-09-09 00:46:53 +0700

    base config

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {store} from "./store/store";    //transfer data stored in a common workspace
+import {Provider} from "react-redux";  
+import { BrowserRouter } from "react-router-dom";
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
-    <App></App>
+  <BrowserRouter>
+              <Provider store={store}>
+                  <App />
+              </Provider>
+            </BrowserRouter>
+  
+   
 
 );
 
